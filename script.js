9d91801611a023542cee8c21153cdbf2beb7ea67
BigChe02 <terwave12345678999@gmail.com>
2025-02-01 00:02:25 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+let audio = null;
+let isFirstClick = true;
+
 function openCard() {
     const card = document.querySelector('.card');
     card.classList.toggle('open');
+    
+    document.body.classList.toggle('card-opened');
+    if (card.classList.contains('open')) {
+        if (isFirstClick) {
+            audio = new Audio('sound-effect.mp3');
+            audio.loop = false;
+            audio.volume = 0.5;
+            audio.play();
+            isFirstClick = false;
+        }
+    } else {
+        if (audio) {
+            audio.pause();
+            audio.currentTime = 0;
+        }
+        isFirstClick = true;
+    }
 }
